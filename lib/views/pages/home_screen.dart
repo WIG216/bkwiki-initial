9b4ad3ce14465140d98ee86bb8c24d863841c901
WIG216 <numbucliff04@gmail.com>
2023-02-16 16:09:58 +0100

    updates

--- lib/views/pages/home_screen.dart
+++ lib/views/pages/home_screen.dart
@@ -59,7 +59,9 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               AppBarIcons(
                 function: () {
-                  
+                 
+                },
+                icon: IconlyBold.activity,
               ),
             ],
           ),
